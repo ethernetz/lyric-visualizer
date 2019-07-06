dc3d6b0a77a49dd7902e43c38c333eb7820feb86
Ethernetz <Ethannetz@hotmail.com>
2019-07-05 19:49:45 -0700

    Progress bar for tutorial made

--- src/app/tutorial/tutorial.component.ts
+++ src/app/tutorial/tutorial.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
+import { map, takeWhile } from 'rxjs/operators';
 
 @Component({
     selector: 'app-tutorial-component',
     templateUrl: './tutorial.component.html',
-    styleUrls: []
+    styleUrls: ['./tutorial.component.scss']
 })
 
 
 export class TutorialComponent implements OnInit{
 
+    
     ngOnInit(){
+        const progressBar = document.getElementById('progress');
+        const durationInSeconds = 15
+        const duration = (durationInSeconds*100)/2;
+        const progress = interval(20).pipe(
+            map(progress => progress/duration),
+            takeWhile(ratio => ratio < 1)
+        ).subscribe(ratio => progressBar.style.width = ratio*100 + '%')
+
+
         localStorage.setItem('new_user', 'false');
     }
 
