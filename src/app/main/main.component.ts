5dce833fd117543a73a2eebee523028b76d9c477
damanm24 <dmulye2@illinois.edu>
2019-07-13 02:03:06 -0700

    Updated tutorial component

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -19,15 +19,16 @@ export class MainComponent implements OnInit {
     ngOnInit(): void {
         var string = "I'm a Barbie girl in a Barbie world".split(" ")
         var gridData = [];
-        var size = this.screenWidth / 25;
-        var offset = this.screenWidth / 15;
+        var size = 60;
         for (var i: number = 0; i < 8; i++) {
             gridData[i] = [];
             for (var j: number = 0; j < 8; j++) {
                 gridData[i][j] = {
                     id: i * 8 + j + 1,
-                    x: (j * size) + 1 + offset,
-                    y: (i * size) + 1 + offset,
+                    i: i,
+                    j: j,
+                    x: (j * size) + 1 + 20,
+                    y: (i * size) + 1 + 20,
                     width: size,
                     height: size,
                     row_name: string[i],
@@ -36,10 +37,14 @@ export class MainComponent implements OnInit {
 
             }
         }
+
         var grid = d3.select("#grid")
             .append("svg")
             .attr("width", "590px")
-            .attr("height", "590px");
+            .attr("height", "590px")
+            .attr("id", "grid-svg");
+
+        //var filter = d3.select("#grid-svg").append("defs").append("filter").append("feFlood").attr("flood-color", "yellow").append("feComposite").attr()
 
         var row = grid.selectAll(".row")
             .data(gridData)
@@ -54,15 +59,25 @@ export class MainComponent implements OnInit {
             .attr("y", function (d: any) { return d.y; })
             .attr("width", function (d: any) { return d.width; })
             .attr("height", function (d: any) { return d.height; })
-            .style("fill", "#252425")
+            .style("fill", (d : any) => {
+                if (d.i === d.j && (d.row_name != "a" || d.row_name == "Barbie")) {
+                    return "grey"
+                } else if (d.row_name === "Barbie" && d.col_name === "Barbie") {
+                    return "#eb6774"
+                } else if (d.col_name === "a" && d.row_name === "a") {
+                    return "#34c8e9"
+                } else {
+                    return "#252425"
+                }
+            })
             .style("stroke", "grey");
 
         row.append("text").attr("x", 0).attr("y", 0).attr("dy", ".32em").attr("text-anchor", "middle").text((d, i) => { return d[i].row_name }).attr("style", (d, i) => {
-            return `transform: translate(85px, ${d[i].y + 30}px)rotate(90deg); fill:white; font: 18px sans-serif;`;
+            return `transform: translate(7px, ${d[i].y + 30}px)rotate(90deg); fill:white; font: 18px sans-serif;`;
         });
 
         row.append("text").attr("x", 0).attr("y", 0).attr("dy", ".32em").attr("text-anchor", "middle").text((d : any, i) => { return d[i].col_name }).attr("style", (d, i : any) => {
-            return `transform: translate(${d[i].x + 30}px, 85px); fill:white; font: 18px sans-serif;`;
+            return `transform: translate(${d[i].x + 30}px, 7px); fill:white; font: 18px sans-serif;`;
         });
     }
 
