9b0bdaa28e1d3dc43b5b0db2fd4fdbc0d7cd4ad4
damanm24 <dmulye2@illinois.edu>
2019-07-13 00:54:49 -0700

    Partial tutorial graph

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3';
 
 @Component({
     selector: 'app-main',
@@ -7,4 +7,50 @@ import { Component } from '@angular/core';
     styleUrls: ['./main.component.scss']
 })
 
-export class MainComponent { }
+export class MainComponent implements OnInit {
+
+    ngOnInit(): void {
+        var string = "I'm a Barbie girl in a Barbie world".split(" ")
+        var gridData = [];
+        var size = 50;
+        for(var i: number = 0; i < 8; i++) {
+            gridData[i] = [];
+            for(var j: number = 0; j< 8; j++) {
+                gridData[i][j] = {
+                    id: i * 10 + j + 1,
+                    x: (j * 50) + 1 + 100,
+                    y: (i * 50) + 1 + 100,
+                    width: size,
+                    height: size,
+                    row_name: string[i],
+                    col_name: string[j]
+                }
+                
+            }
+        }
+        var x =  d3.scaleOrdinal().range([0, 8*50 + 1]);
+        var grid = d3.select("#grid")
+            .append("svg")
+            .attr("width", "1000px")
+            .attr("height", "1000px");
+            
+        var row = grid.selectAll(".row")
+            .data(gridData)
+            .enter().append("g")
+            .attr("class", "row");
+
+        var column = row.selectAll(".square")
+            .data(function (d : any) { return d; })
+            .enter().append("rect")
+            .attr("class", "square")
+            .attr("x", function (d : any) { return d.x; })
+            .attr("y", function (d : any) { return d.y; })
+            .attr("width", function (d : any) { return d.width; })
+            .attr("height", function (d : any) { return d.height; })
+            .style("fill", "#252425")
+            .style("stroke", "grey");
+
+        
+    }
+
+}
